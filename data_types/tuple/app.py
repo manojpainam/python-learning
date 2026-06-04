36178f78b8a4d3fefe77e032866b8924d6d858a3
Manoj P <manojpainam1221@gmail.com>
2026-06-04 23:06:58 +0530

    Enhance tuple operations: add item updating, packing, and unpacking examples

--- data_types/tuple/app.py
+++ data_types/tuple/app.py
@@ -38,3 +38,35 @@ print(multi_data_type_tuple[-1])
 
 #range indexing
 print(multi_data_type_tuple[0:2])
+
+#if item exists in the tuple
+if "manoj" in multi_data_type_tuple:
+    print("Hey, 'manoj' exists in the tuple")
+
+'''
+Updating tuple - Since tuples are immutable we can't add or delete elements from the tuple
+instead we can do the below to do the same - convert the tuple to list and then add element to the list and then change back list to tuple
+even have to do the same for the removing as well
+'''
+fruits = list(thistuple)
+fruits[1] = "kiwi"
+thistuple = tuple(fruits)
+
+print(thistuple)
+
+
+'''
+Packaging and unpacking tuples
+'''
+
+#packing
+names = ("manoj", "vamshi", "purna", "prasad")
+#unpacking
+(eleder_son, younger_son, wife, me) = names
+print(eleder_son)
+
+#if the number of values are greater then number of total values of the tuple then use * to get the rest of the values as list
+states = ("maharastra", "kerala", "andhra pradesh", "telangana", "tamilnadu", "west bengal")
+(mp, *southern_states, wb) = states
+
+print(southern_states)
