93e70decb461fddb0757840002ea1870a38a86f3
Manoj P <manojpainam1221@gmail.com>
2026-06-09 21:00:55 +0530

    Enhance set operations: add examples for looping, joining, intersection, difference, and symmetric difference

--- data_types/set/app.py
+++ data_types/set/app.py
@@ -75,5 +75,82 @@ del education_data
 #as we have deleted this varibale above this will produce an error
 #print(education_data)
 
+#looping sets
+heros = {"Ram Charan", "Pawan Kalyan", "Jr NTR", "Allu Arjun"}
+for hero in heros:
+    print(hero)
 
+'''
+Joining sets
+'''
+#As we know that set doesn't allow duplicates it will ignore if dupliactes exists
+mid_heros = {"Ram Charan", "Varun Sandeesh", "Sai Dharam Tej"}
 
+#union method is used to combine the 2 sets
+#union will create a new set
+combined_heros = heros.union(mid_heros)
+print(combined_heros)
+
+alphabets = {"a", "b", "c", "d"}
+numbers = {1, 2, 3, 4}
+
+#Also we can use | operator to work same as union
+combined_data = alphabets | numbers
+print(combined_data)
+
+#can join multiple sets using union method also we can also use | to join the same
+whole_data = combined_heros.union(combined_data, thisset)
+print(whole_data)
+
+#update method | it will not create a new set instead it can update the existing set
+#heros.update(mid_heros)
+#print(heros)
+
+#intersection will keep the only duplicate elements | will return a new set
+new_set = heros.intersection(mid_heros)
+print(new_set)
+
+#also we can use & to perform the same intersection method
+common_elements = heros & mid_heros
+print(common_elements)
+
+#just like the intersection method it will show only the common element but instead of new set it will show in the existing elememt
+# heros.intersection_update(mid_heros)
+# print(heros)
+
+#Difference method will return the elements from the first elements which doesn't exists in the second set
+new_heros_set = heros.difference(mid_heros)
+print(new_heros_set)
+
+# - operator will give the same result as difference
+new_heros_set_using_hyphen = heros - mid_heros
+print(new_heros_set_using_hyphen)
+
+set1 = {"apple", "banana", "cherry"}
+set2 = {"google", "microsoft", "apple"}
+
+set1.difference_update(set2)
+
+print(set1)
+
+new_set_of_heros = heros.symmetric_difference(mid_heros)
+print(new_set_of_heros)
+
+#we can also use ^ to perform the same symmetric_difference
+new_set_of_heros_using_cap = heros ^ mid_heros
+print(new_set_of_heros_using_cap)
+
+set3 = {"apple", "banana", "cherry"}
+set4 = {"google", "microsoft", "apple"}
+
+#to keep the items that are not present in both sets
+set3.symmetric_difference_update(set4)
+
+print(set3)
+
+'''
+frozen set - unlike sets fronset doesn't allow to create or delete the elements
+'''
+x = frozenset({"pondicherry", "tiruvanmalai", "banglore", "Hyderabad"})
+print(x)
+print(type(x))
