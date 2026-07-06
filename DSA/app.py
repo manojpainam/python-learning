e65f57987a046138f39d3f44ddddc02ec7442335
Manoj P <manoj.painam1@gmail.com>
2026-07-06 23:05:41 +0530

    Adde a binary serach algorithm

--- DSA/app.py
+++ DSA/app.py
@@ -232,3 +232,22 @@ if result != -1:
     print("Element found at index :", result)
 else:
     print("Element not found")
+
+def binarySearch(list, target):
+    low = 0
+    high = len(list) - 1
+
+    while low <= high:
+        mid = low + high // 2
+
+        if list[mid] == target:
+            return mid
+        
+        #narrow down search area
+        if list[mid] < target:
+            low = mid + 1
+        else:
+            high = mid - 1
+
+    return -1
+
