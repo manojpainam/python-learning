e18827d8ba63a11093222e8bc69b2e96cca0217c
Manoj P <manoj.painam1@gmail.com>
2026-07-25 21:22:07 +0530

    Add maxProduct function to calculate the maximum product of any two digits in a number

--- leetcode/leetcode.py
+++ leetcode/leetcode.py
@@ -548,3 +548,61 @@ def can_win_nim(n: int) -> bool:
     return n % 4 != 0
 
 print("can win nim:", can_win_nim(3))
+
+
+'''
+You are given a positive integer n.
+
+Return the maximum product of any two digits in n.
+
+Note: You may use the same digit twice if it appears more than once in n.
+
+ 
+
+Example 1:
+
+Input: n = 31
+
+Output: 3
+
+Explanation:
+
+The digits of n are [3, 1].
+The possible products of any two digits are: 3 * 1 = 3.
+The maximum product is 3.
+Example 2:
+
+Input: n = 22
+
+Output: 4
+
+Explanation:
+
+The digits of n are [2, 2].
+The possible products of any two digits are: 2 * 2 = 4.
+The maximum product is 4.
+Example 3:
+
+Input: n = 124
+
+Output: 8
+
+Explanation:
+
+The digits of n are [1, 2, 4].
+The possible products of any two digits are: 1 * 2 = 2, 1 * 4 = 4, 2 * 4 = 8.
+The maximum product is 8.
+'''
+def maxProduct(n: int) -> int:
+    max_product = 0
+    str_num = str(n)
+
+    for i in range(len(str_num)):
+        for j in range(i+1, len(str_num)):
+            product = int(str_num[i]) * int(str_num[j])
+            if product > max_product:
+                max_product = product
+
+    return max_product
+
+print("max_product", maxProduct(124))
