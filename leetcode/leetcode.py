3964a91e14906c76ac51665e0fc1b6a547282e73
Manoj P <manoj.painam1@gmail.com>
2026-07-31 23:22:54 +0530

    Add search_insert function to find the index for target value in a sorted array with examples

--- leetcode/leetcode.py
+++ leetcode/leetcode.py
@@ -869,3 +869,48 @@ n = 3
 merge(nums1, m, nums2, n)
 
 print(nums1)
+
+'''
+Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+
+You must write an algorithm with O(log n) runtime complexity.
+
+ 
+
+Example 1:
+
+Input: nums = [1,3,5,6], target = 5
+Output: 2
+Example 2:
+
+Input: nums = [1,3,5,6], target = 2
+Output: 1
+Example 3:
+
+Input: nums = [1,3,5,6], target = 7
+Output: 4
+ 
+
+Constraints:
+
+1 <= nums.length <= 104
+-104 <= nums[i] <= 104
+nums contains distinct values sorted in ascending order.
+-104 <= target <= 104
+'''
+
+def search_insert(nums: List[int], target: int) -> int:
+    left, right = 0, len(nums) - 1
+
+    while left <= right:
+        mid = left + (right - left) // 2
+
+        if nums[mid] == target:
+            return mid
+        elif nums[mid] > target:
+            right = mid - 1
+        else:
+            left = mid + 1
+    return left
+
+print("Insert postion is :", search_insert([1, 2, 4, 5, 6], 3))
